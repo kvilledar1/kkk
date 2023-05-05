d08f3925ed5f7d9dab4b79e1ca2bd1f3f97ff773
kimberly villeda <kvilledar1@miumg.com.edu>
2023-05-05 00:03:20 -0600

    calculadora

--- main.cpp
+++ main.cpp
@@ -16,3 +16,81 @@ int main() {
 
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operacion;
+    float num1, num2;
+    cout << "Ingrese la operacion (+, -, *, /): ";
+    cin >> operacion;
+    cout << "Ingrese los dos numeros: ";
+    cin >> num1 >> num2;
+
+    switch(operacion){
+        case '+':
+            cout << "Resultado: " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Resultado: " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Resultado: " << num1 * num2;
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                cout << "Resultado: " << num1 / num2;
+            } else {
+                cout << "No se puede dividir por cero";
+            }
+            break;
+
+        default:
+            cout << "Operacion no valida";
+            break;
+    }
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operacion;
+    float num1, num2;
+    cout << "Ingrese la operacion (+, -, *, /): ";
+    cin >> operacion;
+    cout << "Ingrese los dos numeros: ";
+    cin >> num1 >> num2;
+
+    switch(operacion){
+        case '+':
+            cout << "Resultado: " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Resultado: " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Resultado: " << num1 * num2;
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                cout << "Resultado: " << num1 / num2;
+            } else {
+                cout << "No se puede dividir por cero";
+            }
+            break;
+
+        default:
+            cout << "Operacion no valida";
+            break;
+    }
+
+    return 0;
+}
